2e3ac13d6c8b1b9a9bc8995e7152d7ffca11b80e
setohe0909 <setohe.09@gmail.com>
2021-07-04 15:31:19 -0500

    feat:new rout look

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -4,26 +4,25 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Page404 from '../404';
 import RegisterUser from '../Register';
+import LookUser from '../look';
 import withContext from '../../HOC/withContext';
 
-const Main = () => {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path="/" exact>
-            <RegisterUser />
-          </Route>
-          <Route path="/integrations" exact>
-            <div>2</div>
-          </Route>
-          <Route>
-            <Page404 />
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
-};
+const Main = () => (
+  <BrowserRouter>
+    <div className="App">
+      <Switch>
+        <Route path="/" exact>
+          <RegisterUser />
+        </Route>
+        <Route path="/look" exact>
+          <LookUser />
+        </Route>
+        <Route>
+          <Page404 />
+        </Route>
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
 
 export default withContext(Main);
