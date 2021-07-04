a88e8780a7baf4cd735b1330f270f93d81205c9c
setohe0909 <setohe.09@gmail.com>
2021-07-04 15:15:12 -0500

    feat: 404 page

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+
+import NotFound from '../../assets/img/404.png';
 import { Page404Container } from './styles';
 
 const Page404 = () => {
-  return <Page404Container>hello</Page404Container>;
+  return (
+    <Page404Container>
+      <strong>No encontramos lo que buscabas.</strong>
+      <img alt="Not found" src={NotFound} />
+    </Page404Container>
+  );
 };
 
 export default Page404;
--- src/components/404/styles.js
+++ src/components/404/styles.js
@@ -1,8 +1,28 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+import { mq } from '../layout/utils/base';
+import { Colors } from '../ui/Colors';
 
 export const Page404Container = styled.div`
-  height: calc(100% - 275px);
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  padding: 20px;
+  height: 96vh;
+  background: ${Colors.white};
 
-  i {
+  img {
+    width: 400px;
   }
+
+  ${mq.md(css`
+    width: 60%;
+    height: auto;
+    border-radius: 8px;
+    margin: 20px auto;
+  `)};
 `;
